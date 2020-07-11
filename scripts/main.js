b7b1ee27f9c88527de286e7e2198a0f526ed399d
StevenLzn <stevenlozano17@gmail.com>
2020-07-11 13:20:52 -0500

    Agregada funcionalidad de operaciones basicas, suma,resta,multiplicacion y division

--- scripts/main.js
+++ scripts/main.js
@@ -1,24 +1,79 @@
+var num1 = '';
+var num2 = '';
+var currentOperation = '';
+var isNextValue = false;
+
 function clickNumber(number) {
-    var value = document.getElementById("display").value;
-    value = value + number;
-    document.getElementById("display").value = value;
+    if (isNextValue) {
+        num2 = num2.toString() + number
+        document.getElementById("display").value = num2;
+    } else {
+        value = document.getElementById("display").value;
+        num1 = num1.toString() + number;
+        document.getElementById("display").value = num1;
+    }
 };
+
+function operations(operation) {
+    var result = 0;
+    switch (operation) {
+        case 'x':
+            result = num1 * num2
+            break;
+        case '÷':
+            result = num1 / num2
+            break;
+        case '+':
+            result = parseInt(num1) + parseInt(num2)
+            break;
+        case '-':
+            result = num1 - num2
+            break;
+        default:
+            break;
+    }
+    return result;
+}
+
+function result(operation) {
+    if (num1 != 0 && num2 != 0 && operation != '=') {
+        num2 = document.getElementById("display").value;
+        num1 = document.getElementById("display").value = this.operations(currentOperation);
+        currentOperation = operation;        
+        num2 = '';
+        
+    } else if(num1 != 0 && num2 == 0 && operation != '='){
+        num1 = document.getElementById("display").value;
+        currentOperation = operation;
+        isNextValue = true;
+    }
+    if (operation == '=') {
+        num2 = document.getElementById("display").value;
+        num1 = document.getElementById("display").value = this.operations(currentOperation);  
+        num2 = '';
+    }
+
+}
 
 function clickOperation(operation) {
     switch (operation) {
         case 'x':
-            alert("Es una multiplicación")
+            this.result('x')
             break;
         case '÷':
-            alert("Es una división")
+            this.result('÷')
             break;
         case '+':
-            alert("Es una suma")
+            this.result('+')
             break;
         case '-':
-            alert("Es una resta")
+            this.result('-')
+            break;
+        case '=':
+            this.result('=')
             break;
         default:
             break;
     }
 };
+
