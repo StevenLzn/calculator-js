68ba5347006e12a7b82f39dfc5d823451dbd62b2
StevenLzn <stevenlozano17@gmail.com>
2020-07-15 11:11:10 -0500

    Agregada funcionalidad de limpiar, cambiar signo, agregada pantalla donde se ve la operacion que se esta haciendo, solucionados otros errores.

--- scripts/main.js
+++ scripts/main.js
@@ -2,15 +2,17 @@ var num1 = '';
 var num2 = '';
 var currentOperation = '';
 var isNextValue = false;
+var cleared = false;
 
 function clickNumber(number) {
     if (isNextValue) {
         num2 = num2.toString() + number
-        document.getElementById("display").value = num2;
+        document.getElementById("display-num").value = num2;
+        cleared = false;
     } else {
-        value = document.getElementById("display").value;
+        value = document.getElementById("display-num").value;
         num1 = num1.toString() + number;
-        document.getElementById("display").value = num1;
+        document.getElementById("display-num").value = num1;
     }
 };
 
@@ -47,36 +49,39 @@ function result(operation) {
     //alert(currentOperation)
     if (num1 != 0 && num2 != 0 && operation != '=' && operation != '=' && operation != '^') {
         //alert("1")
-        num2 = document.getElementById("display").value;
-        num1 = document.getElementById("display").value = this.operations(currentOperation);
+        num2 = document.getElementById("display-num").value;
+        num1 = document.getElementById("display-num").value = this.operations(currentOperation);
         currentOperation = operation;
         num2 = '';
     } else if (num1 != 0 && num2 == 0 && operation != '=' && operation != '^' && operation != '√') {
         //alert("2")
-        num1 = document.getElementById("display").value;
+        num1 = document.getElementById("display-num").value;
         currentOperation = operation;
         isNextValue = true;
     } else if (num1 != 0 &&  currentOperation == '' && operation == '^' || operation == '√' ) {
         //alert("3")
         currentOperation = operation;
-        num1 = document.getElementById("display").value = this.operations(currentOperation);
+        num1 = document.getElementById("display-num").value = this.operations(currentOperation);
         currentOperation = '';
-        isNextValue = true;
+        isNextValue = false;
     } else if (num1 != 0 && currentOperation != '' && operation == '^' || operation == '√' ) {
         //alert("4")
         num1 =  this.operations(currentOperation);
         currentOperation = operation;
-        num1 = document.getElementById("display").value = this.operations(currentOperation);
+        num1 = document.getElementById("display-num").value = this.operations(currentOperation);
         currentOperation = '';
-        isNextValue = true;
+        num2 = ''
+        isNextValue = false;
     }
-    if (operation == '=' && currentOperation != '') {
-        num2 = document.getElementById("display").value;
-        num1 = document.getElementById("display").value = this.operations(currentOperation);
+    if (operation == '=' && currentOperation != '' && num2 > 0) {
+        num2 = document.getElementById("display-num").value;
+        num1 = document.getElementById("display-num").value = this.operations(currentOperation);
         currentOperation = '';
         num2 = '';
+        isNextValue = false;
     }
 
+    document.getElementById("display-op").value = num1 + ' ' + currentOperation + ' ' + num2
 }
 
 function clickOperation(operation) {
@@ -107,3 +112,26 @@ function clickOperation(operation) {
     }
 };
 
+function changeSign(){
+    document.getElementById("display-num").value *= -1;
+}
+
+function clearDisplay(){
+    if(cleared){
+        currentOperation = '';
+        document.getElementById("display-op").value = '';
+        document.getElementById("display-num").value = '';
+        num1 = '';
+        num2 = '';
+        isNextValue = false;
+        cleared = false;
+    }else if(!cleared && currentOperation == ''){
+        document.getElementById("display-num").value = '';
+        document.getElementById("display-op").value = '';
+        num1 = '';  
+    }else if (!cleared && num2 > 0){
+        document.getElementById("display-num").value = '';
+        num2 = '';
+        cleared = true;
+    }
+}
