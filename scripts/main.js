44b3bc751dfbe31cbaedcda6b2d4b1db9062bdb1
StevenLzn <stevenlozano17@gmail.com>
2020-07-16 11:31:02 -0500

    Agregadas funcioens trigonometricas, agregada funcion de porcentaje, arreglados otros errores

--- scripts/main.js
+++ scripts/main.js
@@ -5,6 +5,12 @@ var isNextValue = false;
 var cleared = false;
 
 function clickNumber(number) {
+    if (num1 == 0) {
+        num1 = ''
+    } else if (num2 == 0) {
+        num2 = ''
+    }
+
     if (isNextValue) {
         num2 = num2.toString() + number
         document.getElementById("display-num").value = num2;
@@ -26,7 +32,7 @@ function operations(operation) {
             result = num1 / num2
             break;
         case '+':
-            result = parseInt(num1) + parseInt(num2)
+            result = parseFloat(num1) + parseFloat(num2)
             break;
         case '-':
             result = num1 - num2
@@ -36,6 +42,9 @@ function operations(operation) {
             break;
         case '√':
             result = Math.sqrt(num1)
+            break;
+        case '%':
+            result = (num1 * num2)/100
             break;
         default:
             break;
@@ -47,26 +56,26 @@ function result(operation) {
     //alert(num1)
     //alert(num2)
     //alert(currentOperation)
-    if (num1 != 0 && num2 != 0 && operation != '=' && operation != '=' && operation != '^') {
+    if (num1 != '' && num2 != '' && operation != '=' && operation != '=' && operation != '^') {
         //alert("1")
         num2 = document.getElementById("display-num").value;
         num1 = document.getElementById("display-num").value = this.operations(currentOperation);
         currentOperation = operation;
         num2 = '';
-    } else if (num1 != 0 && num2 == 0 && operation != '=' && operation != '^' && operation != '√') {
+    } else if (num1 != '' && num2 == '' && operation != '=' && operation != '^' && operation != '√') {
         //alert("2")
         num1 = document.getElementById("display-num").value;
         currentOperation = operation;
         isNextValue = true;
-    } else if (num1 != 0 &&  currentOperation == '' && operation == '^' || operation == '√' ) {
+    } else if (num1 != '' && currentOperation == '' && operation == '^' || operation == '√') {
         //alert("3")
         currentOperation = operation;
         num1 = document.getElementById("display-num").value = this.operations(currentOperation);
         currentOperation = '';
         isNextValue = false;
-    } else if (num1 != 0 && currentOperation != '' && operation == '^' || operation == '√' ) {
+    } else if (num1 != '' && currentOperation != '' && operation == '^' || operation == '√') {
         //alert("4")
-        num1 =  this.operations(currentOperation);
+        num1 = this.operations(currentOperation);
         currentOperation = operation;
         num1 = document.getElementById("display-num").value = this.operations(currentOperation);
         currentOperation = '';
@@ -107,17 +116,41 @@ function clickOperation(operation) {
         case '√':
             this.result('√')
             break;
+        case '%':
+            this.result('%')
+            break;
         default:
             break;
     }
 };
 
-function changeSign(){
-    document.getElementById("display-num").value *= -1;
+function clickTrigo(value) {
+    if (num1 != '') {
+        switch (value) {
+            case 'cos':
+                num1 = document.getElementById("display-num").value = Math.cos(num1);
+                break;
+            case 'sin':
+                num1 = document.getElementById("display-num").value = Math.sin(num1);
+                break;
+            case 'tan':
+                num1 = document.getElementById("display-num").value = Math.tan(num1);
+                break;
+
+            default:
+                break;
+        }
+    }
+};
+
+function changeSign() {
+    if (num1 != 0) {
+        document.getElementById("display-num").value *= -1;
+    }
 }
 
-function clearDisplay(){
-    if(cleared){
+function clearDisplay() {
+    if (cleared) {
         currentOperation = '';
         document.getElementById("display-op").value = '';
         document.getElementById("display-num").value = '';
@@ -125,11 +158,11 @@ function clearDisplay(){
         num2 = '';
         isNextValue = false;
         cleared = false;
-    }else if(!cleared && currentOperation == ''){
+    } else if (!cleared && currentOperation == '') {
         document.getElementById("display-num").value = '';
         document.getElementById("display-op").value = '';
-        num1 = '';  
-    }else if (!cleared && num2 > 0){
+        num1 = '';
+    } else if (!cleared && num2 > 0) {
         document.getElementById("display-num").value = '';
         num2 = '';
         cleared = true;
